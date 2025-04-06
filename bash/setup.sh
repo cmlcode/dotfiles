7ed1d10705ae44c68c2ed1fa2edae6ae1c633f4a
Chris Lewis <61091285+cmlcode@users.noreply.github.com>
2025-04-06 15:08:11 -0500

    Improve error handling (#1)
    * Add error handling
    
    * Update setup.sh
    
    * Update setup.sh
    
    * Added error handling

--- bash/setup.sh
+++ bash/setup.sh
@@ -1,15 +1,45 @@
 #!/bin/bash
+set -e 
 echo "Setting up bash..."
+
+# Default args
+DRY_RUN=FALSE
+
+# Parse args
+for arg in "$@"; do
+  case $arg in
+    --dry-run)
+        DRY_RUN=true
+            ;;
+    *)
+        echo "Unknown  option: $arg"
+            exit 1
+      ;;
+      esac
+done
+
 DIR="${PWD}/bash"
-COPYDIR=$HOME
+COPYDIR="${HOME}"
+
 shopt -s dotglob
-for file in "$DIR"/*;
-do
-	filename=$(basename "$file")
-	if [[ "$filename" == "setup.sh" ]]; then
-		continue
-	fi
-	copypath="${COPYDIR}/${filename}"
-	cp ${file} ${copypath}
+for file in "$DIR"/*; do
+  filename=$(basename "${file}")
+ 
+  [[ "${filename}" == "setup.sh" ]] && continue 
+
+  src="${file}"
+  dest="${COPYDIR}/${filename}"
+
+  if [[ -f "${dest}" ]]; then
+    if cmp -s "$src" "$dest"; then
+      continue # No changes, skip file
+    fi
+  fi 
+  if [[ "$DRY_RUN" == true ]]; then
+    echo "-> [dry-run] Would copy ${filename} to ${dest}"
+  else
+    echo "-> Copying ${filename} to ${dest}"
+    cp ${src} ${dest}
+  fi
 done
 shopt -u dotglob
--- setup.sh
+++ setup.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
-for DIR in $(find ${PWD} -maxdepth 1); do
-	if [[ "$DIR" == "$PWD" ]]; then
-		continue
+
+set -e
+
+# Capture ARGS
+ARGS="$@"
+
+for DIR in $(find ${PWD} -mindepth 1 -maxdepth 1 -type d); do
+	SETUP_FILE="${DIR}/setup.sh"
+	if [[ -f "${SETUP_FILE}" ]]; then
+		bash "${SETUP_FILE}" ${ARGS}
+  	else
+   		echo "SKIPPING ${DIR}. Error: Setup file ${SETUP_FILE} not found"
 	fi
-	DIR+="/setup.sh"
-	test -f $DIR && bash $DIR
 done
-echo "Finished install"
+echo "Finished install process"
