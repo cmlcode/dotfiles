00f1221c36a3b7ca086f7a31cc50210538a8b6b6
Chris Lewis <61091285+cmlcode@users.noreply.github.com>
2025-04-06 13:31:23 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,12 +2,15 @@
 
 set -e
 
-echo "Starting dotfiles installation..."
+# Capture ARGS
+ARGS="$@"
 
 for DIR in $(find ${PWD} -mindepth 1 -maxdepth 1 type d); do
 	SETUP_FILE="${DIR}/setup.sh"
- 	if [[ -f "$SETUP_FILE" ]]; then
-  		bash "$SETUP_FILE"
-    	fi
+	if [[ -f "${SETUP_FILE}" ]]; then
+		bash "${SETUP_FILE}" ${ARGS}
+  	else
+   		echo "SKIPPING ${DIR}. Error: Setup file ${SETUP_FILE} not found"
+	fi
 done
-echo "Finished all installs"
+echo "Finished install process"
