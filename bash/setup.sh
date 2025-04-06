6cc742f69cc089b14e764c9ff12b3ffbfce73ef7
Christopher Lewis <61091285+cmlcode@users.noreply.github.com>
2025-04-06 15:07:36 -0500

    Added error handling

--- bash/setup.sh
+++ bash/setup.sh
@@ -3,19 +3,19 @@ set -e
 echo "Setting up bash..."
 
 # Default args
-DRY_RUN = FALSE
+DRY_RUN=FALSE
 
 # Parse args
 for arg in "$@"; do
-	case $arg in
- 		--dry-run)
-   			DRY_RUN=true
-      			;;
-	 	*)
-   			echo "Unknown  option: $arg"
-      			exit 1
-	 		;;
-    	esac
+  case $arg in
+    --dry-run)
+        DRY_RUN=true
+            ;;
+    *)
+        echo "Unknown  option: $arg"
+            exit 1
+      ;;
+      esac
 done
 
 DIR="${PWD}/bash"
@@ -23,24 +23,23 @@ COPYDIR="${HOME}"
 
 shopt -s dotglob
 for file in "$DIR"/*; do
-	filename=$(basename "${file}")
+  filename=$(basename "${file}")
  
-	[[ "${filename}" == "setup.sh" ]] && continue 
+  [[ "${filename}" == "setup.sh" ]] && continue 
 
-   	src="${file}"
-	dest="${COPYDIR}/${filename}"
+  src="${file}"
+  dest="${COPYDIR}/${filename}"
 
- 	if [[ -f "${dest}" ]]; then
-  		if cmp -s "$src" "$dest"; then
-    			continue # No changes, skip file
-       		fi
-	 fi
-  
- 	if [[ "$DRY_RUN" == true ]]; then
-  		echo "-> [dry-run] Would copy ${filename} to ${target}"
-    	else
-     		echo "-> Copying ${filename} to ${target}"
-		cp ${src} ${dest}
-	fi
+  if [[ -f "${dest}" ]]; then
+    if cmp -s "$src" "$dest"; then
+      continue # No changes, skip file
+    fi
+  fi 
+  if [[ "$DRY_RUN" == true ]]; then
+    echo "-> [dry-run] Would copy ${filename} to ${dest}"
+  else
+    echo "-> Copying ${filename} to ${dest}"
+    cp ${src} ${dest}
+  fi
 done
 shopt -u dotglob
--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ set -e
 # Capture ARGS
 ARGS="$@"
 
-for DIR in $(find ${PWD} -mindepth 1 -maxdepth 1 type d); do
+for DIR in $(find ${PWD} -mindepth 1 -maxdepth 1 -type d); do
 	SETUP_FILE="${DIR}/setup.sh"
 	if [[ -f "${SETUP_FILE}" ]]; then
 		bash "${SETUP_FILE}" ${ARGS}
