7fb1ce7d27c1a97ddeb3220a1637e755e805ba4f
Chris Lewis <61091285+cmlcode@users.noreply.github.com>
2025-05-21 11:29:16 -0500

    Add hyprland setup file

---
+++ hypr/setup.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+set -e 
+
+# -----------------------
+# Dotfiles Setup Template
+# -----------------------
+
+# Default args
+APP_NAME="$(basename "${BASH_SOURCE[0]%/*}")"
+DRY_RUN=false
+SETUP_DIR="${PWD}/${APP_NAME}"
+TARGET_DIR="${HOME}/.config/${APP_NAME}"
+
+# Parse args
+for arg in "$@"; do
+  case $arg in
+    --dry-run)
+        DRY_RUN=true
+            ;;
+    *)
+        echo "Unknown  option: $arg"
+            exit 1
+      ;;
+      esac
+done
+
+echo "Setting up ${APP_NAME}..."
+
+shopt -s dotglob
+for file in "${SETUP_DIR}"/*; do
+  filename=$(basename "${file}")
+ 
+  [[ "${filename}" == "setup.sh" ]] && continue 
+
+  src="${file}"
+  dest="${TARGET_DIR}/${filename}"
+
+  # Compare files
+  if [[ -e "${dest}" ]]; then
+    if diff -qr "$src" "$dest" > /dev/null; then
+      continue 
+    fi
+  fi 
+
+  if [[ "$DRY_RUN" == true ]]; then
+    # Dry run
+    echo "-> [dry-run] Would copy ${filename} to ${dest}"
+  else
+    # Run copy
+    echo "-> Copying ${filename} to ${dest}"
+		rm -rf "${dest}"
+    mkdir -p "$(dirname "${dest}")"
+		cp -r "${src}" "${dest}"
+  fi
+done
+
+shopt -u dotglob
+echo "Finished setting up ${APP_NAME}" 
